6b005fb095659712dc773b684ef94afc8a406775
Patrick Nielsen <archi1990@hotmail.com>
2017-07-24 01:03:31 +0200

    Menu, added colors (red, green, blue, white and black)

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour {
 
     public List<Button> buttonsPressed;
+    public int vibrationDuration;
 	// Use this for initialization
 	void Start () {
         buttonsPressed = new List<Button>();
@@ -18,11 +20,24 @@ public class ButtonController : MonoBehaviour {
 
     public void OnButtonPressed(Button button)
     {
-        if (buttonsPressed.Count != 2 && !buttonsPressed.Contains(button))
+        if ( !buttonsPressed.Contains(button))
         {
+            //Handheld.Vibrate();
+            Vibration.Vibrate(vibrationDuration);
             buttonsPressed.Add(button);
             Color color = button.GetComponent<Image>().color;
             button.GetComponent<Image>().color = new Color(color.r, color.g, color.b, 0.5f);
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<TouchTest>().playerBall.GetComponent<MeshRenderer>().material.color += color;
         }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
--- Assets/Scripts/TouchTest.cs
+++ Assets/Scripts/TouchTest.cs
@@ -11,66 +11,63 @@ public class TouchTest : MonoBehaviour {
     public GameObject Player;
     public Vector3 spawnPosition;
     public GameObject Buttons;
+    public GameObject playerBall;
+    public int vibrationDuration;
 
-    public List<Color> colors = new List<Color>();
     void Update()
     {
+        if (playerBall == null)
+        {
+            Quaternion spawnRotation = Quaternion.identity;
+            playerBall = Instantiate(Player, new Vector3(spawnPosition.x, spawnPosition.y, spawnPosition.z), spawnRotation);
+            playerBall.GetComponent<MeshRenderer>().material.color = Color.black;
+            Debug.Log("Player Created");
+        }
+
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
-            Vector3 target = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).deltaPosition);
+           // Vector3 target = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).deltaPosition);
             transform.position = new Vector3(target.x, target.y, transform.position.z);
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && target.y >= playerBall.transform.position.y)
         {
-            Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vibration.Vibrate(vibrationDuration);
+            playerBall.GetComponent<PlayerBallMoving>().startMoving = true;
             GameObject go = GameObject.FindGameObjectWithTag("Buttons");
             List<Button> buttons = go.GetComponent<ButtonController>().buttonsPressed;
 
-            if (buttons.Count == 2)
+            //Quaternion spawnRotation = Quaternion.identity;
+            //GameObject playerBall = Instantiate(Player, new Vector3(spawnPosition.x, spawnPosition.y, spawnPosition.z), spawnRotation);
+            GameObject[] playerBalls = GameObject.FindGameObjectsWithTag("PlayerBall");
+            playerBalls[playerBalls.Length - 1].GetComponent<PlayerBallMoving>().Target = target;
+            playerBalls[playerBalls.Length - 1].GetComponent<PlayerBallMoving>().vector = playerBalls[playerBalls.Length - 1].GetComponent<PlayerBallMoving>().Target - playerBalls[playerBalls.Length - 1].transform.position;
+            playerBalls[playerBalls.Length - 1].GetComponent<PlayerBallMoving>().vector.Normalize();
+
+            Color ballColor = Color.black;
+            if (buttons.Count > 0)
             {
-                Quaternion spawnRotation = Quaternion.identity;
-                GameObject playerBall = Instantiate(Player, new Vector3(spawnPosition.x, spawnPosition.y, spawnPosition.z), spawnRotation);
-                GameObject[] playerBalls = GameObject.FindGameObjectsWithTag("PlayerBall");
-                playerBalls[playerBalls.Length - 1].GetComponent<PlayerBallMoving>().Target = target;
-
                 for (int i = 0; i < buttons.Count; i++)
                 {
-                    colors.Add(buttons[i].image.color);
+                    ballColor += buttons[i].image.color;
                 }
-
-                Color red = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
-                Color green = new Color(Color.green.r, Color.green.g, Color.green.b, 0.5f);
-                Color blue = new Color(Color.blue.r, Color.blue.g, Color.blue.b, 0.5f);
-
-
-                if (colors.Contains(red) && colors.Contains(green))
-                {
-                    Debug.Log("yellow");
-                    playerBall.GetComponent<MeshRenderer>().material.color = Color.yellow;
-                    playerBall.tag = "Color.yellow";
-                }
-                if (colors.Contains(green) && colors.Contains(blue))
-                {
-                    Debug.Log("cyan");
-                    playerBall.GetComponent<MeshRenderer>().material.color = Color.cyan;
-                    playerBall.tag = "Color.cyan";
-                }
-                if (colors.Contains(red) && colors.Contains(blue))
-                {
-                    Debug.Log("magenta");
-                    playerBall.GetComponent<MeshRenderer>().material.color = Color.magenta;
-                    playerBall.tag = "Color.magenta";
-                }
-                for (int i = 0; i < buttons.Count; i++)
-                {
-                    Color color = go.GetComponent<ButtonController>().buttonsPressed[i].image.color;
-                    go.GetComponent<ButtonController>().buttonsPressed[i].image.color = new Color(color.r, color.g, color.b, 1f);
-                }
-                buttons.Clear();
-                colors.Clear();
-                go.GetComponent<ButtonController>().buttonsPressed.Clear();
             }
+            ballColor.a = 1;
+            playerBall.GetComponent<MeshRenderer>().material.color = ballColor;
+            playerBall.tag = ballColor.ToString(); 
+            playerBall.GetComponent<BallColor>().ballColor = playerBall.GetComponent<MeshRenderer>().material.color;
+            //Debug.Log(ballColor.ToString());
+
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                Color color = go.GetComponent<ButtonController>().buttonsPressed[i].image.color;
+                go.GetComponent<ButtonController>().buttonsPressed[i].image.color = new Color(color.r, color.g, color.b, 1f);
+            }
+            buttons.Clear();
+            go.GetComponent<ButtonController>().buttonsPressed.Clear();
+            playerBall = null;
         }
     }
 }
