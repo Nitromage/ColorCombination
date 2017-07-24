c96394c8066873c714c231d24beee800e133c7e7
Zackian <steffentidus@hotmail.com>
2017-07-25 00:47:46 +0200

    new scene with CMYK colors

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -21,14 +21,19 @@ public class GameController : MonoBehaviour
     float waveTimer;
     bool startWaveTimer;
     int enemyCountIncrement = 1;
-    List<Color> colors = new List<Color>() { Color.green, Color.red, Color.blue, Color.black, Color.white };
+    // List<Color> colors = new List<Color>() { Color.green, Color.red, Color.blue, Color.black, Color.white };
+    List<Color> colors = new List<Color>() { Color.magenta, new Color(1,1,0,1), Color.cyan, Color.black, new Color(2,2,2,3) };
     // Use this for initialization
     void Start()
     {
+        
         //SpawnWave();
-        colors.Add(Color.red + Color.green);
-        colors.Add(Color.red + Color.blue);
-        colors.Add(Color.green + Color.blue);
+        /* colors.Add(Color.red + Color.green);
+         colors.Add(Color.red + Color.blue);
+         colors.Add(Color.green + Color.blue);*/
+        colors.Add(Color.magenta + new Color(1,1,0,1));
+        colors.Add(Color.magenta + Color.cyan);
+        colors.Add(new Color(1,1,0,1) + Color.cyan);
     }
 
     IEnumerator SpawnWave()
